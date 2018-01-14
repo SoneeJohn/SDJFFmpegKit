402faca034586793f7332c140fa634236fd34748
Soneé John <sonee@alphasoftware.co>
2018-01-14 00:20:04 -0400

    Add Xcode project

---
+++ SDJFFmpegKit/SDJFFmpegKit.h
@@ -0,0 +1,19 @@
+//
+//  SDJFFmpegKit.h
+//  SDJFFmpegKit
+//
+//  Created by Soneé John on 1/14/18.
+//  Copyright © 2018 Soneé John. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SDJFFmpegKit.
+FOUNDATION_EXPORT double SDJFFmpegKitVersionNumber;
+
+//! Project version string for SDJFFmpegKit.
+FOUNDATION_EXPORT const unsigned char SDJFFmpegKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDJFFmpegKit/PublicHeader.h>
+
+
